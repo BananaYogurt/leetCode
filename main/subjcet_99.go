9a4ade4f34f10ac8db4f635fa593d4ac3cbaf5ae
wzh <373038132@qq.com>
2019-12-16 17:07:25 +0800

    99 二叉树恢复  草稿2

--- main/subjcet_99.go
+++ main/subjcet_99.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 //99. 恢复二叉搜索树
 
 type TreeNode struct {
@@ -12,54 +10,80 @@ type TreeNode struct {
 
 type TmpStack struct {
 	top   int
-	nodes [8]*TreeNode
+	nodes [8]*TmpNode
 }
 
 type TmpNode struct {
-	node *TreeNode
-	left bool
+	node     *TreeNode
+	nodeType int // 0 根 ； 1 右边 ； -1 左边
 }
 
-func (stack *TmpStack) push(node *TreeNode) {
+func (stack *TmpStack) push(node *TmpNode) {
 	stack.top++
 	stack.nodes[stack.top] = node
 }
 
-func (stack *TmpStack) peek() *TreeNode {
+func (stack *TmpStack) peek() *TmpNode {
 	node := stack.nodes[stack.top]
 	stack.top--
 	return node
 }
 
+func (tmpNode *TmpNode) getLeft() *TreeNode {
+	return tmpNode.node.Left
+}
+
+func (tmpNode *TmpNode) getRight() *TreeNode {
+	return tmpNode.node.Right
+}
+
 func recoverTree(root *TreeNode) {
 	var n1 *TreeNode
 	var n2 *TreeNode
-	stack := TmpStack{-1, [8]*TreeNode{}}
+	stack := TmpStack{-1, [8]*TmpNode{}}
 	if root != nil {
 		rootV := root.Val
-		stack.push(root)
+		stack.push(&TmpNode{root, 0})
 		for stack.top > -1 {
 			curNode := stack.peek()
-			left := curNode.Left
-			right := curNode.Right
+			left := curNode.getLeft()
+			right := curNode.getRight()
 			ok := true
 			if nil != right {
-				stack.push(right)
-				if right.Val < curNode.Val {
+				if curNode.nodeType == 0 {
+					stack.push(&TmpNode{right, 1})
+				} else {
+					stack.push(&TmpNode{right, curNode.nodeType})
+				}
+				if right.Val < curNode.node.Val {
 					ok = false
 				}
 			}
 			if nil != left {
-				if left.Val > curNode.Val {
+				if curNode.nodeType == 0 {
+					stack.push(&TmpNode{left, -1})
+				} else {
+					stack.push(&TmpNode{left, curNode.nodeType})
+				}
+				if left.Val > curNode.node.Val {
 					ok = false
 				}
-				stack.push(left)
 			}
+			if curNode.nodeType == -1 { //左子树的节点值全部小于根结点的值
+				if curNode.node.Val > rootV {
+					ok = false
+				}
+			} else if curNode.nodeType == 1 { //右子树的节点值全部大于根结点的值
+				if curNode.node.Val < rootV {
+					ok = false
+				}
+			}
+
 			if !ok {
 				if n1 == nil {
-					n1 = curNode
+					n1 = curNode.node
 				} else {
-					n2 = curNode
+					n2 = curNode.node
 					break
 				}
 			}
@@ -70,10 +94,10 @@ func recoverTree(root *TreeNode) {
 	}
 }
 
-func main() {
+/*func main() {
 	stack := TmpStack{-1, [8]*TreeNode{}}
 	node := TreeNode{1, nil, nil}
 	stack.push(&node)
 	n1 := stack.peek()
 	fmt.Println("运行结果", n1.Val)
-}
+}*/
